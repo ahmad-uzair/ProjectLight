c010bbe9016570409e48a8cc0dc78a554b42bdea
uzzu96 <ahmad_uzair000@yahoo.co.in>
2018-01-16 16:29:37 +0530

    Add project files.

---
+++ APITester/APITester/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace APITester
+{
+    public partial class MainPage : ContentPage
+    {
+        HttpClient client;
+        public MainPage()
+        {
+            InitializeComponent();
+            client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            const string RestUrl = "https://projectlight.onlosant.com/gpio/2";
+            var uri = new Uri(string.Format(RestUrl, string.Empty));
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                //var content = await response.Content.ReadAsStringAsync();
+                //ResultLabel.Text = content;
+            }
+            const string RestUrl2 = "https://projectlight.onlosant.com/devices/5a5233088cc82100078f0dd6/data";
+            var uri2 = new Uri(string.Format(RestUrl2, string.Empty));
+            var response2 = await client.GetAsync(uri2);
+            if (response2.IsSuccessStatusCode)
+            {
+                var content2 = await response2.Content.ReadAsStringAsync();
+                ResultLabel.Text = content2;
+            }
+        }
+
+        async private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            const string RestUrl = "https://projectlight.onlosant.com/gpio/3";
+            var uri = new Uri(string.Format(RestUrl, string.Empty));
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                //ResultLabel.Text = content;
+            }
+        }
+
+        async private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            const string RestUrl = "https://projectlight.onlosant.com/gpio/4";
+            var uri = new Uri(string.Format(RestUrl, string.Empty));
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                //ResultLabel.Text = content;
+            }
+        }
+
+        async private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            const string RestUrl = "https://projectlight.onlosant.com/gpio/17";
+            var uri = new Uri(string.Format(RestUrl, string.Empty));
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                //ResultLabel.Text = content;
+            }
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_7(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ APITester/APITester/testAPI.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace APITester
+//{
+//    class testAPI
+//    {
+//        const string BaseUrl = "https://api.losant.com";
+
+//        readonly string _accountSid;
+//        readonly string _secretKey;
+
+//        public testAPI(string accountSid, string secretKey)
+//        {
+//            _accountSid = accountSid;
+//            _secretKey = secretKey;
+//        }
+
+//        public T Execute<T>(RestRequest request) where T : new()
+//        {
+//            var client = new RestClient();
+//            client.BaseUrl = new System.Uri(BaseUrl);
+//            client.Authenticator = new HttpBasicAuthenticator(_accountSid, _secretKey);
+//            request.AddParameter("AccountSid", _accountSid, ParameterType.UrlSegment); // used on every request
+//            var response = client.Execute<T>(request);
+
+//            if (response.ErrorException != null)
+//            {
+//                const string message = "Error retrieving response.  Check inner details for more info.";
+//                var twilioException = new ApplicationException(message, response.ErrorException);
+//                throw twilioException;
+//            }
+//            return response.Data;
+//        }
+//    }
+//}
